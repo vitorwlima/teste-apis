03a131c7120fc2187cbf8237f9c019503922d5c7
vitorwlima <vitorwlima13@gmail.com>
2021-04-27 14:18:06 -0300

    estilo adicionado à página

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
-const form = document.forms.cep;
-const btnSubmit = form.querySelector("#submit");
-const info = document.querySelectorAll("p");
-let cepInserido;
+function scriptCep() {
+  const form = document.forms.cep;
+  const btnSubmit = form.querySelector("#submit");
+  const info = document.querySelectorAll("p");
+  let cepInserido;
 
-btnSubmit.addEventListener("click", alterarInfo);
-form.addEventListener("change", handleChange);
+  btnSubmit.addEventListener("click", alterarInfo);
+  form.addEventListener("change", handleChange);
 
-function handleChange(e) {
-  cepInserido = +e.target.value;
+  function handleChange(e) {
+    cepInserido = +e.target.value;
+  }
+
+  function alterarInfo(action) {
+    action.preventDefault();
+    const pCep = document.querySelector(".cep");
+    const pCidade = document.querySelector(".cidadeUF");
+    const pDdd = document.querySelector(".ddd");
+    const pBairro = document.querySelector(".bairro");
+    const pRua = document.querySelector(".logradouro");
+    fetch(`https://viacep.com.br/ws/${cepInserido}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        pCep.innerText = `CEP: ${cepInserido}`;
+        pCidade.innerText = `Cidade - UF: ${data.localidade} - ${data.uf}`;
+        pDdd.innerText = `DDD: ${data.ddd}`;
+        pBairro.innerText = `Bairro: ${data.bairro}`;
+        pRua.innerText = `Rua: ${data.logradouro}`;
+      })
+      .catch(() => {
+        pCep.innerText = "CEP: Insira um CEP válido.";
+        pCidade.innerText = "Cidade:";
+        pDdd.innerText = "DDD:";
+        pBairro.innerText = "Bairro:";
+        pRua.innerText = "Rua:";
+      });
+  }
 }
-
-function alterarInfo(action) {
-  action.preventDefault();
-  const pCep = document.querySelector(".cep");
-  const pCidade = document.querySelector(".cidadeUF");
-  const pDdd = document.querySelector(".ddd");
-  const pBairro = document.querySelector(".bairro");
-  const pRua = document.querySelector(".logradouro");
-  fetch(`https://viacep.com.br/ws/${cepInserido}/json/`)
-    .then((res) => res.json())
-    .then((data) => {
-      pCep.innerText = `CEP: ${cepInserido}`;
-      pCidade.innerText = `Cidade - UF: ${data.localidade} - ${data.uf}`;
-      pDdd.innerText = `DDD: ${data.ddd}`;
-      pBairro.innerText = `Bairro: ${data.bairro}`;
-      pRua.innerText = `Rua: ${data.logradouro}`;
-    })
-    .catch(() => {
-      pCep.innerText = "CEP: Insira um CEP válido.";
-      pCidade.innerText = "Cidade:";
-      pDdd.innerText = "DDD:";
-      pBairro.innerText = "Bairro:";
-      pRua.innerText = "Rua:";
-    });
-}
+scriptCep();
--- js/scriptCep.js
+++
@@ -1,3 +0,0 @@
-// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
-// crie um formulário onde o usuário pode digitar o cep
-// e o endereço completo é retornado ao clicar em buscar
