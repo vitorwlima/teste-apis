91463a937812d54167554a26afdb5db35e94a50d
vitorwlima <vitorwlima13@gmail.com>
2021-04-27 13:50:57 -0300

    formulário com integração da API dos ceps 100% funcional

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 const form = document.forms.cep;
 const btnSubmit = form.querySelector("#submit");
+const info = document.querySelectorAll("p");
 let cepInserido;
+
+btnSubmit.addEventListener("click", alterarInfo);
+form.addEventListener("change", handleChange);
 
 function handleChange(e) {
   cepInserido = +e.target.value;
 }
 
-btnSubmit.addEventListener("click", alterarInfo);
-
-form.addEventListener("change", handleChange);
-
 function alterarInfo(action) {
   action.preventDefault();
-  const teste = document.querySelector("p");
+  const pCep = document.querySelector(".cep");
+  const pCidade = document.querySelector(".cidadeUF");
+  const pDdd = document.querySelector(".ddd");
+  const pBairro = document.querySelector(".bairro");
+  const pRua = document.querySelector(".logradouro");
   fetch(`https://viacep.com.br/ws/${cepInserido}/json/`)
     .then((res) => res.json())
-    .then((data) => (teste.innerText = data.logradouro));
+    .then((data) => {
+      pCep.innerText = `CEP: ${cepInserido}`;
+      pCidade.innerText = `Cidade - UF: ${data.localidade} - ${data.uf}`;
+      pDdd.innerText = `DDD: ${data.ddd}`;
+      pBairro.innerText = `Bairro: ${data.bairro}`;
+      pRua.innerText = `Rua: ${data.logradouro}`;
+    })
+    .catch(() => {
+      pCep.innerText = "CEP: Insira um CEP válido.";
+      pCidade.innerText = "Cidade:";
+      pDdd.innerText = "DDD:";
+      pBairro.innerText = "Bairro:";
+      pRua.innerText = "Rua:";
+    });
 }
